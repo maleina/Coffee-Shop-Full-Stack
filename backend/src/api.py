8299e23e3909b5d09ecbb2b79ce9321ecbb4b26d
Maleina Bidek <maleina@ucla.edu>
2019-12-27 11:20:55 -0500

    refactor: Removed unneccessary error handling, updated comments.

--- backend/src/api.py
+++ backend/src/api.py
@@ -12,7 +12,7 @@ setup_db(app)
 CORS(app)
 
 '''
-@DONE uncomment the following line to initialize the database
+@DONE Uncomment the following line to initialize the database.
 !! NOTE THIS WILL DROP ALL RECORDS AND START YOUR DB FROM SCRATCH
 !! NOTE THIS MUST BE UNCOMMENTED ON FIRST RUN
 '''
@@ -22,7 +22,7 @@ CORS(app)
 
 # ROUTES
 
-# temporary route for testing authorization
+# Temporary route for testing authorization.
 # @app.route('/headers')
 # @requires_auth('post:drinks')
 # def headers(payload):
@@ -31,59 +31,59 @@ CORS(app)
 
 
 '''
-@DONE implement endpoint
+@DONE Implement endpoint
     GET /drinks
-        it should be a public endpoint
-        it should contain only the drink.short() data representation
-    returns status code 200 and json {"success": True, "drinks": drinks} where drinks is the list of drinks
-        or appropriate status code indicating reason for failure
+        It should be a public endpoint.
+        It should contain only the drink.short() data representation.
+    Returns status code 200 and json {"success": True, "drinks": drinks} where drinks is the list of drinks
+        or appropriate status code indicating reason for failure.
 '''
 
 
 @app.route('/drinks')
 def retrieve_drinks():
-    # Retrieve drinks and format them correctly
+    # Retrieve drinks and format them correctly.
     selection = Drink.query.all()
     drinks = [drink.short() for drink in selection]
     # Abort if there are no drinks in the database,
-    # otherwise, return all drinks in short form
+    # otherwise, return all drinks in short form.
     if len(drinks) == 0:
         abort(404)
     return jsonify({'success': True, "drinks": drinks})
 
 
 '''
-@DONE implement endpoint
+@DONE Implement endpoint
     GET /drinks-detail
-        it should require the 'get:drinks-detail' permission
-        it should contain the drink.long() data representation
-    returns status code 200 and json {"success": True, "drinks": drinks} where drinks is the list of drinks
-        or appropriate status code indicating reason for failure
+        It should require the 'get:drinks-detail' permission.
+        It should contain the drink.long() data representation.
+    Returns status code 200 and json {"success": True, "drinks": drinks} where drinks is the list of drinks
+        or appropriate status code indicating reason for failure.
 '''
 
 
 @app.route('/drinks-detail')
 @requires_auth('get:drinks-detail')
 def retrieve_drink_detail(payload):
-    # Retrieve drinks and format them correctly
+    # Retrieve drinks and format them correctly.
     selection = Drink.query.all()
     drinks = [drink.long() for drink in selection]
 
     # Abort if there are no drinks in the database,
-    # otherwise, return all drinks in long form
+    # otherwise, return all drinks in long form.
     if len(drinks) == 0:
         abort(404)
     return jsonify({"success": True, "drinks": drinks})
 
 
 '''
-@DONE implement endpoint
+@DONE Implement endpoint
     POST /drinks
-        it should create a new row in the drinks table
-        it should require the 'post:drinks' permission
-        it should contain the drink.long() data representation
-    returns status code 200 and json {"success": True, "drinks": drink} where drink an array containing only the newly created drink
-        or appropriate status code indicating reason for failure
+        It should create a new row in the drinks table.
+        It should require the 'post:drinks' permission.
+        It should contain the drink.long() data representation.
+    Returns status code 200 and json {"success": True, "drinks": drink} where drink an array containing only the newly created drink
+        or appropriate status code indicating reason for failure.
 '''
 
 
@@ -91,44 +91,43 @@ def retrieve_drink_detail(payload):
 @requires_auth('post:drinks')
 def create_drink(payload):
     try:
-        # Get new drink data from request
+        # Get new drink data from request.
         body = request.get_json()
         req_title = body.get('title', None)
         req_recipe = body.get('recipe', None)
 
-        # Validate that title and recipe are both present
-        # If not, abort
+        # Validate that title and recipe are both present,
+        # if not, abort.
         if (req_title is None) or (req_recipe is None):
             return abort(422)
 
         # If the recipe has not been input as a list,
-        # format it correctly and create the drink object
+        # format it correctly and create the drink object.
         if type(req_recipe) is not list:
             req_recipe = [req_recipe]
         drink = Drink(title=req_title, recipe=json.dumps(req_recipe))
 
-        # Abort if the drink is already present in the database
+        # Abort if the drink is already present in the database.
         if drink.is_duplicate():
             abort(422)
 
-        # Otherwise, create a row in the database for the drink
+        # Otherwise, create a row in the database for the drink.
         drink.insert()
         return jsonify({'success': True, "drinks": [drink.long()]})
     except:
         abort(422)
 
 
-
 '''
-@DONE implement endpoint
+@DONE Implement endpoint
     PATCH /drinks/<id>
         where <id> is the existing model id
-        it should respond with a 404 error if <id> is not found
-        it should update the corresponding row for <id>
-        it should require the 'patch:drinks' permission
-        it should contain the drink.long() data representation
-    returns status code 200 and json {"success": True, "drinks": drink} where drink an array containing only the updated drink
-        or appropriate status code indicating reason for failure
+        It should respond with a 404 error if <id> is not found.
+        It should update the corresponding row for <id>.
+        It should require the 'patch:drinks' permission.
+        It should contain the drink.long() data representation.
+    Returns status code 200 and json {"success": True, "drinks": drink} where drink an array containing only the updated drink
+        or appropriate status code indicating reason for failure.
 '''
 
 
@@ -137,17 +136,17 @@ def create_drink(payload):
 def update_drink(payload, id):
     try:
         # Find the drink with the given id,
-        # if it doesn't exist abort
+        # if it doesn't exist abort.
         drink = Drink.query.filter(Drink.id == id).one_or_none()
         if drink is None:
             abort(404)
 
-        # Retrieve the updated drink data
+        # Retrieve the updated drink data.
         body = request.get_json()
         req_title = body.get('title', None)
         req_recipe = body.get('recipe', None)
 
-        # Update the drink with the new values
+        # Update the drink with the new values.
         if req_title is not None:
             drink.title = req_title
         if req_recipe is not None:
@@ -161,14 +160,14 @@ def update_drink(payload, id):
 
 
 '''
-@DONE implement endpoint
+@DONE Implement endpoint
     DELETE /drinks/<id>
-        where <id> is the existing model id
-        it should respond with a 404 error if <id> is not found
-        it should delete the corresponding row for <id>
-        it should require the 'delete:drinks' permission
-    returns status code 200 and json {"success": True, "delete": id} where id is the id of the deleted record
-        or appropriate status code indicating reason for failure
+        where <id> is the existing model id.
+        It should respond with a 404 error if <id> is not found.
+        It should delete the corresponding row for <id>.
+        It should require the 'delete:drinks' permission.
+    Returns status code 200 and json {"success": True, "delete": id} where id is the id of the deleted record
+        or appropriate status code indicating reason for failure.
 '''
 
 
@@ -177,7 +176,7 @@ def update_drink(payload, id):
 def delete_drink(payload, id):
     try:
         # Find the drink with the given id,
-        # if it doesn't exist abort
+        # if it doesn't exist abort.
         drink = Drink.query.filter(Drink.id == id).one_or_none()
         if drink is None:
             abort(404)
@@ -190,7 +189,7 @@ def delete_drink(payload, id):
 
 # Error Handling
 '''
-Example error handling for unprocessable entity
+Example error handling for unprocessable entity:
 '''
 
 
@@ -204,8 +203,8 @@ def unprocessable(error):
 
 
 '''
-@DONE implement error handlers using the @app.errorhandler(error) decorator
-    each error handler should return (with approprate messages):
+@DONE Implement error handlers using the @app.errorhandler(error) decorator.
+    Each error handler should return (with appropriate messages):
              jsonify({
                     "success": False, 
                     "error": 404,
@@ -233,18 +232,9 @@ def bad_request(error):
     }), 400
 
 
-@app.errorhandler(403)
-def forbidden(error):
-    return jsonify({
-        "success": False,
-        "error": 403,
-        "message": "resource forbidden"
-    }), 403
-
-
 '''
-@DONE implement error handler for 404
-    error handler should conform to general task above 
+@DONE Implement error handler for 404.
+    Error handler should conform to general task above.
 '''
 
 
@@ -258,8 +248,8 @@ def not_found(error):
 
 
 '''
-@DONE implement error handler for AuthError
-    error handler should conform to general task above 
+@DONE Implement error handler for AuthError.
+    Error handler should conform to general task above.
 '''
 
 
--- backend/src/auth/auth.py
+++ backend/src/auth/auth.py
@@ -1,5 +1,5 @@
 import json
-from flask import request, _request_ctx_stack, abort
+from flask import request, abort
 from functools import wraps
 from jose import jwt
 from urllib.request import urlopen
@@ -8,10 +8,9 @@ AUTH0_DOMAIN = 'mcbcoffee.auth0.com'
 ALGORITHMS = ['RS256']
 API_AUDIENCE = 'drinks'
 
-## AuthError Exception
+# AuthError Exception
 '''
-AuthError Exception
-A standardized way to communicate auth failure modes
+AuthError Exception is a standardized way to communicate auth failure modes.
 '''
 
 
@@ -24,18 +23,17 @@ class AuthError(Exception):
 # Auth Header
 
 '''
-@TODO implement get_token_auth_header() method
-    it should attempt to get the header from the request
-        it should raise an AuthError if no header is present
-    it should attempt to split bearer and the token
-        it should raise an AuthError if the header is malformed
-    return the token part of the header
+@DONE Implement get_token_auth_header() method.
+    It should attempt to get the header from the request.
+        It should raise an AuthError if no header is present.
+    It should attempt to split bearer and the token.
+        It should raise an AuthError if the header is malformed.
+    Return the token part of the header.
 '''
 
 
 def get_token_auth_header():
-    """Obtains the Access Token from the Authorization Header
-    """
+    # Obtains the Access Token from the Authorization Header.
     auth = request.headers.get('Authorization', None)
     if not auth:
         raise AuthError({
@@ -67,15 +65,15 @@ def get_token_auth_header():
 
 
 '''
-@TODO implement check_permissions(permission, payload) method
+@DONE Implement check_permissions(permission, payload) method.
     @INPUTS
         permission: string permission (i.e. 'post:drink')
         payload: decoded jwt payload
 
-    it should raise an AuthError if permissions are not included in the payload
+    It should raise an AuthError if permissions are not included in the payload.
         !!NOTE check your RBAC settings in Auth0
-    it should raise an AuthError if the requested permission string is not in the payload permissions array
-    return true otherwise
+    It should raise an AuthError if the requested permission string is not in the payload permissions array.
+    Return true otherwise.
 '''
 
 
@@ -90,20 +88,20 @@ def check_permissions(permission, payload):
         raise AuthError({
             'code': 'unauthorized',
             'description': 'Permission not found.'
-        }, 403)
+        }, 401)
     return True
 
 
 '''
-@TODO implement verify_decode_jwt(token) method
+@DONE Implement verify_decode_jwt(token) method.
     @INPUTS
         token: a json web token (string)
 
-    it should be an Auth0 token with key id (kid)
-    it should verify the token using Auth0 /.well-known/jwks.json
-    it should decode the payload from the token
-    it should validate the claims
-    return the decoded payload
+    It should be an Auth0 token with key id (kid).
+    It should verify the token using Auth0 /.well-known/jwks.json.
+    It should decode the payload from the token.
+    It should validate the claims.
+    Return the decoded payload.
 
     !!NOTE urlopen has a common certificate error described here: https://stackoverflow.com/questions/50236117/scraping-ssl-certificate-verify-failed-error-for-http-en-wikipedia-org
 '''
@@ -166,14 +164,14 @@ def verify_decode_jwt(token):
 
 
 '''
-@TODO implement @requires_auth(permission) decorator method
+@DONE Implement @requires_auth(permission) decorator method.
     @INPUTS
         permission: string permission (i.e. 'post:drink')
 
-    it should use the get_token_auth_header method to get the token
-    it should use the verify_decode_jwt method to decode the jwt
-    it should use the check_permissions method validate claims and check the requested permission
-    return the decorator which passes the decoded payload to the decorated method
+    It should use the get_token_auth_header method to get the token.
+    It should use the verify_decode_jwt method to decode the jwt.
+    It should use the check_permissions method validate claims and check the requested permission.
+    Return the decorator which passes the decoded payload to the decorated method.
 '''
 
 
